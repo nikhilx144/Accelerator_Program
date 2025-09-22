b312deec8eb09dc660c81442cec1b4ebb2bbd903
Nikhil <nikhilgelli1@gmail.com>
2025-09-22 22:03:30 +0530

    finished tasks 9 and 10 in html css bootcamp

---
+++ JS_Devops/JS_Day1/app.js
@@ -0,0 +1,36 @@
+// Exercise 1
+/* alert("I'm Javascript"); */
+
+// Exercise 2
+/* let name = "G. Nikhil";
+let admin = name;
+alert(admin); */
+
+// Exercise 3
+/* let a = Number(prompt("Enter the first number"));
+let b = Number(prompt("Enter the second number"));
+alert(a + b); */
+
+// Exercise 4
+/* let username = prompt("Enter your username: ", "")
+if (username == "admin") {
+    let password = prompt("Enter your password: ");
+
+    if (password == "root") {
+        alert("Welcome!");
+    }
+    else if (password == "" || password == null) {
+        alert("Please enter a password. Try again");
+    }
+    else {
+        alert("Password is incorrect. Login failed!")
+    }
+}
+
+else if (username == "" || username == null) {
+    alert("Please enter your username. Try again");
+}
+
+else {
+    alert("Username not recognized!")
+} */
